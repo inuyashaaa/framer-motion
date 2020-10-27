7309425e2fce2eeba09b02bca6d0df1edda2768c
Huy Manh <huymanhtmhp@gmail.com>
2020-10-28 00:09:05 +0700

    Add keyframe

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,20 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const buttonVariants = {
+  // visible: {
+  //   x: [0, -20, 20, -20, 20, 0],
+  //   transition: {
+  //     delay: 2
+  //   }
+  // },
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1],
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+  }
+}
+
 const Home = () => {
   return (
     <motion.div
@@ -15,11 +29,9 @@ const Home = () => {
       </h2>
       <Link to="/base">
         <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: "0px 0px 8px rgb(255,255,255)",
-            boxShadow: "0px 0px 8px rgb(255,255,255)",
-          }}
+          variants={buttonVariants}
+          animate="visible"
+          whileHover="hover"
         >
           Create Your Pizza
         </motion.button>
